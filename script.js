b5af140cc4574d40eb0329a3d52a331669901a4b
loremispum2k8 <141552551+loremispum2k8@users.noreply.github.com>
2025-02-17 19:01:06 +0200

    Speed"

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ left.addEventListener('click',(e)=>{
                 clearInterval(i);
             }
 
-            Translation -= 15;
+            Translation -= 5;
             carouselInnerContainer.style.transform = `translateX(-${Translation}px)`
         },1.5);
     }
@@ -75,7 +75,7 @@ right.addEventListener('click',(e)=>{
                 clearInterval(i);
             }
 
-            Translation += 15;
+            Translation += 5;
             carouselInnerContainer.style.transform = `translateX(-${Translation}px)`
         },1.5);
 
